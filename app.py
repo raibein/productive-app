0dc101402763d1f043e266aa15d8413f8b837596
raben shrestha <raben@rabens-MBP.askey.com>
2023-07-24 13:24:06 -0500

    udpate code

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from src.api_registers import main
 
 if __name__ == '__main__':
     main()
-    # app.run(debug=True) # only for debuging not for production.
-    app.run(debug=False)
+    app.run(debug=True) # only for debuging not for production.
+    # app.run(debug=False)
