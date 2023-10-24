d303f06355cfe77f537e731ca0b7347a38e057f4
raben shrestha <raben@rabens-MBP.lan>
2023-10-24 15:13:10 -0500

    auto load env file to init file

--- src/__init__.py
+++ src/__init__.py
@@ -5,21 +5,29 @@ from src.database.user import User
 from src.database.project import Project
 from src.database.task import Task
 
+from dotenv import load_dotenv
+
 from src.database import db
 
 
-test_config = None
+# Imports Autoload the Environment Variable
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+path = ROOT_DIR + "/../"
+load_dotenv(os.path.join(path, '.env'))
+
+
+config = None
 
 app = Flask(__name__, instance_relative_config=True)
 
-if test_config is None:
+if config is None:
     app.config.from_mapping(
         SECRET_KEY = os.environ.get("SECRET_KEY"),
         SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB_URI"),
         SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
 else:
-    app.config.from_mapping(test_config)
+    app.config.from_mapping(config)
 
 
 '''
