df067453580fabb43f759235bebd982baf4420f1
raben shrestha <raben@rabens-MBP.lan>
2023-10-24 15:06:38 -0500

    app changes made for production

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from src import app
-from src.api_registers import main
+from src.config.api_registers import main
 
+main()
 
 if __name__ == '__main__':
-    main()
-    app.run(debug=True) # only for debuging not for production.
-    # app.run(debug=False)
+    
+    # For Debug and Development Server
+    # app.run(debug=True) # only for debuging not for production.
+
+    # For Production Server
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
